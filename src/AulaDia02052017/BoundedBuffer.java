ca16805f974ea40bdd5d04a20db6ec3a8deed9b5
hylsonk <hylsonk@gmail.com>
2017-05-02 10:30:13 -0300

    Aula de Monitores dia 02/05/2017

---
+++ src/AulaDia02052017/BoundedBuffer.java
@@ -0,0 +1,50 @@
+package AulaDia02052017;
+
+import javax.management.monitor.Monitor;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * Created by Hylson on 02/05/2017.
+ */
+public abstract class BoundedBuffer extends Monitor {
+    private int fullSlots = 0;
+    private int capacity;
+    private int[] buffer;
+
+    private int in = 0;
+    private int out = 0;
+
+    /**
+     * Perguntar ao Quilular se Condition é o equivalente para ConditionVariable
+     *
+     */
+    private Condition notFull;
+    private Condition notEmpty;
+
+
+    public BoundedBuffer(int size){
+        capacity = size;
+        buffer = new int[size];
+    }
+
+    public void deposit(int value) throws InterruptedException {
+        while (fullSlots == capacity){
+            notFull.await();
+        }
+        buffer[in] = value;
+        in =(in + 1)%capacity;
+        fullSlots++;
+    }
+
+    public void withdraw() throws InterruptedException {
+        while(fullSlots == 0){
+            notEmpty.await();
+        }
+        int value = buffer[out];
+        out = (out + 1)%capacity;
+        fullSlots--;
+    }
+
+
+
+}
