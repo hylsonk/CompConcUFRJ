fd283820ba9dd8ac0607336df32fd7777b36c1d9
hylsonk <hylsonk@gmail.com>
2017-05-16 09:17:55 -0300

    Aula dia 16-05-2017

--- src/AulaDia16052017/Filosofo.java
+++ src/AulaDia16052017/Filosofo.java
@@ -1,8 +1,42 @@
 package AulaDia16052017;
 
+import java.util.Random;
+
 /**
  * Created by Hylson on 16/05/2017.
  */
-public class Filosofo {
-    
+public class Filosofo extends Thread{
+    private int id;
+    public int NumVezes = 100;
+    private Random gen = new Random();
+
+    public Filosofo(int id) {
+        this.id = id;
+    }
+
+    void run(){
+        for (int i = 0; i<NumVezes; i++){
+            msg("Pensando");
+            EsperaAleat(100);
+            msg("Quer comer");
+            princ.mesa.QueroComer(id);
+            msg("Comendo");
+            EsperaAleat(100);
+            msg("Acabou de comer");
+            princ.mesa.AcabeiDeComer();
+
+        }
+    }
+    private void msg(String M){
+        System.out.println("Filosofo "+id+": "+M+".");
+    }
+
+    private void EsperaAleat(int TempoMax){
+        try {
+            sleep(gen.nextInt(TempoMax));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
